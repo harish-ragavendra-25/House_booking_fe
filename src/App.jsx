20622086d4e14ad0da4ddc756122b11e10e2f1c8
Sandhya Balamurali <sandhyabalamurali03@gmail.com>
2025-04-22 09:57:11 +0530

    updated image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import Profile from './components/Profile';
+import PostRoom from "./components/PostRoom";
 function App(){
   return(
-    <div>
-      <Profile/>
-    </div>
+    <>
+      
+      <Router>
+        <Routes>
+          <Route path="/post" element={<PostRoom/>}/>
+          <Route path="/" element={<Profile/>}/>
+        </Routes>
+      </Router>
+    </>
   )
 }
 export default App;
